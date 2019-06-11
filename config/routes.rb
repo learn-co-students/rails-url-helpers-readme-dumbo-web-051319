42afddd8032a40624f2882875f3559bfc5c5cfe7
github username <github email address>
2019-06-10 21:26:53 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+
+  get '/register', to: 'users#new', as: 'register'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :posts, only: [:index, :show]
 end
